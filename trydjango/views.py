121d3ae93e06107c39de2c8d8ca96144e85541c3
Avinash Panikkan <avinkb11@gmail.com>
2023-03-20 19:32:16 +0530

    created templates, home & base, used render_to_string, added templates directory

--- trydjango/views.py
+++ trydjango/views.py
@@ -3,7 +3,7 @@ To render html web pages
 """
 import random
 from django.http import HttpResponse
-
+from django.template.loader import render_to_string
 from articles.models import Article 
 
 def home_view(request):
@@ -16,16 +16,18 @@ def home_view(request):
     random_id = random.randint(1,3)
     article_obj = Article.objects.get(id=random_id)
 
-    context = {
+    contexts = {
+        "object":article_obj,
         "title": article_obj.title,
         "id": article_obj.id,
         "content": article_obj.content
     }
 
     #Django Templates
-    HTML_STRING = """
-    <h1>{title} ({id})!</h1>
-    <p>{content}</p>
-    """.format(**context)
+    HTML_STRING = render_to_string("home-view.html",context=contexts)
+    # HTML_STRING = """
+    # <h1>{title} ({id})!</h1>
+    # <p>{content}</p>
+    # """.format(**context)
 
     return HttpResponse(HTML_STRING)
